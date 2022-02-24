88b2fd853426e7d3e435255e29c7c9133ba8c583
KASHYAP <kashyapamipara22@gmail.com>
2022-02-24 12:32:34 +0530

    init commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,108 +1,210 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'admin/admin_first_screen.dart';
+import 'cart.dart';
+import 'intro.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/services.dart';
+import 'user/restuarants.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'package:provider/provider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:custom_splash/custom_splash.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+
+
+import 'dart:async';
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
+  await Firebase.initializeApp();
+  runApp((MyApp()));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  _MyAppState createState() => _MyAppState();
+}
 
-      title: 'Flutter Demo',
-      theme: ThemeData(
+class _MyAppState extends State<MyApp> {
+  bool isoffline=false;
+  final Connectivity _connectivity = Connectivity();
+  StreamSubscription< ConnectivityResult > _connectivitySubscription;
 
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+  @override
+  void initState() {
+    initConnectivity();
+    _connectivitySubscription =
+        _connectivity.onConnectivityChanged.listen(_UpdateConnectionState);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _connectivitySubscription.cancel();
+    super.dispose();
+  }
+
+
+  Future< void > initConnectivity() async {
+    ConnectivityResult result;
+    try {
+      result = await _connectivity.checkConnectivity();
+      setState(() {
+        if(result==ConnectivityResult.none){
+          isoffline=true;
+        }
+        else {
+          isoffline=false;
+        }
+      });
+
+    } on Exception catch (e) {
+      print("Error Occurred: ${e.toString()} ");
+      return;
+    }
+    if (!mounted) {
+      return Future.value(null);
+    }
+    return _UpdateConnectionState(result);
+  }
+
+  Future<void> _UpdateConnectionState(ConnectivityResult result) async {
+    if (result == ConnectivityResult.mobile ||
+        result == ConnectivityResult.wifi) {
+      showStatus(result, true);
+    } else {
+      showStatus(result, false);
+    }
+  }
+
+  void showStatus(ConnectivityResult result, bool status) {
+    if(status){
+      setState(() {
+        isoffline=false;
+      });
+    }
+    else {
+      setState(() {
+        isoffline=true;
+      });
+    }
+
+
+  }
+
+  Widget GK() {
+    return CustomSplash(
+      imagePath: 'asset/logo.png',
+      backGroundColor: Colors.white,
+      // backGroundColor: Color(0xfffc6042),
+      animationEffect: 'zoom-in',
+      logoSize: 800,
+      home: (user == null
+          ? Intro()
+          : (admin ? AdminFirstScreen(key) : Restuarants(key))),
+      // customFunction: await getData(),
+      duration: 2500,
+      type: CustomSplashType.StaticDuration,
+
+      // outputAndHome: op,
     );
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  var user = FirebaseAuth.instance.currentUser;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  var allData;
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  var key;
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  var allData1;
+  int i = 0;
+  bool admin = false;
+  bool isLoad = false;
+  Future<void> getData() async {
+    print(FirebaseAuth.instance.currentUser.email);
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      isLoad = true;
+    });
+    CollectionReference _collectionRef =
+    FirebaseFirestore.instance.collection('user');
+    QuerySnapshot querySnapshot = await _collectionRef.get();
+    allData = querySnapshot.docs.map((doc) => doc.data()).toList();
+    CollectionReference _collectionRef1 =
+    FirebaseFirestore.instance.collection('admin');
+    QuerySnapshot querySnapshot1 = await _collectionRef1.get();
+    allData1 = querySnapshot1.docs.map((doc) => doc.data()).toList();
+    for (int i = 0; i < allData.length; i++) {
+      if (allData[i]['email'] == user.email) {
+        setState(() {
+          key = allData[i]['user_key'];
+          admin = false;
+        });
+      }
+    }
+    for (int i = 0; i < allData1.length; i++) {
+      if (allData1[i]['email'] == user.email) {
+        setState(() {
+          key = allData1[i]['admin_key'];
+          admin = true;
+        });
+      }
+    }
+    print(admin);
+    setState(() {
+      isLoad = false;
+      i = 1;
     });
   }
 
+  bool checkInternet = false;
+  Future<void> check() async {
+    // checkInternet = await InternetConnectionChecker().hasConnection;
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    check();
+    i == 0 ? getData() : null;
+    return MultiProvider(
+      providers: [ChangeNotifierProvider(create: (ctx) => Cart())],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'EATERIA',
+        theme: ThemeData(
+          primarySwatch: Colors.orange,
         ),
+        // home: checkInternet ? GK() : Scaffold(body: Center(child: CircularProgressIndicator(),),),
+        home: isoffline ? Scaffold(
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              // crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  height: 150,
+                  width: 150,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(
+                          "asset/no-internet.png"),
+                      fit: BoxFit.contain,
+                    ),
+                    shape: BoxShape.rectangle,
+                  ),
+                ),
+                SizedBox(height: 20,),
+                Text("NO INTERNET" , style: TextStyle(fontWeight: FontWeight.bold , fontSize: 20),),
+                SizedBox(height: 20,),
+                Text("Turn on your internet connection in order to continue !" , style: TextStyle(fontWeight: FontWeight.bold , fontSize: 10),),
+              ],
+            ),
+          ),
+        ):GK(),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
